cf3f651da3589c98ab50ab88d321daa2fa974176
Shashank Dwivedi <shashank.dwivedi@smarsh.com>
2023-05-22 20:21:29 +0530

    corrected exit codes

--- scan_pr.py
+++ scan_pr.py
@@ -80,5 +80,7 @@ if findings:
     print('Blacklisted words found:')
     for finding in findings:
         print(f"- File: {finding['file']}, Line: {finding['line']}, Word: {finding['word']}")
+        return 1
 else:
     print('No blacklisted words found.')
+    return 0
