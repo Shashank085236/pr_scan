760e80dc0dd9f868119746027042be8aa43356f9
Shashank Dwivedi <shashank.dwivedi@smarsh.com>
2023-05-23 10:53:05 +0530

    test

--- Test/sample.java
+++ Test/sample.java
@@ -1,5 +1,5 @@
 String pass = "secret";
-String pass1 = "secret1";
+String pass1 = "secret123";
 String pass2 = "secret2";
 String pass3 = "secret3";
 public static final String password = "secret";
